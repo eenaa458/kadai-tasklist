2930eb01e1a66fdd5ae9cd71ed491785c1f0b9f5
junki_yokota <potechi458@gmail.com>
2020-05-05 08:00:35 +0000

    relation of user and tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,11 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:show, :create, :update, :delete]
   
   def index
-    @tasks = Task.order(id: :desc).page(params[:page])
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    else
+      redirect_to("/login")
+    end
   end
   
   def show
@@ -11,11 +15,11 @@ class TasksController < ApplicationController
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'タスクが正常に作成されました'
@@ -53,7 +57,7 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
   
   def task_params
