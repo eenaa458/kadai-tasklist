f81632770ed5b35fddf912a528c49f27a8865519
junki_yokota <potechi458@gmail.com>
2020-05-05 12:35:21 +0000

    amend former

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,6 +30,9 @@ class TasksController < ApplicationController
   end
   
   def edit
+    unless @task
+      redirect_to root_url
+    end
   end
   
   def update
@@ -53,7 +56,7 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = current_user.tasks.find(params[:id])
+    @task = current_user.tasks.find_by(params[:id])
   end
   
   def task_params
